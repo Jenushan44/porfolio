95bacdfaf35696567788518ab278fc33855d72d4
Jenushan <jenushan349@gmail.com>
2026-08-04 11:07:16 -0400

    feat: add banner component section

---
+++ app/components/BannerSection.tsx
@@ -0,0 +1,11 @@
+function BannerSection() {
+
+  return (
+
+    <div className="blueprint-bg w-full h-screen border-[#222325] border-2 border-t-black">
+      <p>test</p>
+    </div>
+  )
+}
+
+export default BannerSection
--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,33 @@
-import Navbar from "./components/Navbar"
-import Image from "next/image";
-import Link from "next/link";
-import githubLogo from "./assets/github-logo.png"
-import linkedinLogo from "./assets/linkedin-logo.png"
-import gmailLogo from "./assets/gmail-logo.png"
-import gmailButtonLogo from "./assets/gmail-button-logo.png"
-import { MessageCircle } from 'lucide-react';
+import Navbar from "./components/Navbar";
 import ProjectSection from "./components/ProjectSection";
 import AboutSection from "./components/AboutSection";
 import TechnologySection from "./components/TechnologySection";
 import ExperienceSection from "./components/ExperienceSection";
 import CurrentlyLearningSection from "./components/CurrentlyLearningSection";
 import HomeLabSection from "./components/HomelabSection";
+import BannerSection from "./components/BannerSection";
 
 export default function Home() {
   return (
-    <div className="flex bg-[#0f1112]">
-      <Navbar></Navbar>
+    <div className="blueprint-bg flex text-white">
+      <Navbar />
+
       <main className="flex-1">
+        <BannerSection />
+
         <div className="mb-2 mt-2 flex">
           <AboutSection />
           <TechnologySection />
         </div>
+
         <ExperienceSection />
         <ProjectSection />
+
         <div className="flex">
           <CurrentlyLearningSection />
           <HomeLabSection />
         </div>
       </main>
-
-
-
     </div>
   );
 }
