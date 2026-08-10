5b40f1527606f04269da96a85beb61b4e7031506
Jenushan <jenushan349@gmail.com>
2026-08-09 22:57:47 -0400

    feat: add interactive cursor grid

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,39 @@ import ProjectSection from "./components/ProjectSection";
 import AboutSection from "./components/AboutSection";
 import TechnologySection from "./components/TechnologySection";
 import ExperienceSection from "./components/ExperienceSection";
-import CurrentlyLearningSection from "./components/CurrentlyLearningSection";
 import HomeLabSection from "./components/HomelabSection";
 import BannerSection from "./components/BannerSection";
 import CertificationSection from "./components/CertificationSection";
 import ContactSection from "./components/ContactSection";
+import CursorGrid from "./components/CursorGrid";
 
 export default function Home() {
   return (
-    <div className="blueprint-bg text-white">
-      <Navbar />
+    <div className="text-white">
 
-      <main className="flex-1 mb-20">
-        <BannerSection />
+      <div className="pointer-events-none fixed inset-0 z-11">
+        <CursorGrid cellSize={30} color="#1b88f2" radius={120} falloff="smooth" holdTime={200} fadeDuration={600} lineWidth={2} maxOpacity={1} fillOpacity={0} gridOpacity={0.12} cellRadius={0} clickPulse={true} pulseSpeed={2000} />
+      </div>
 
-        <div className="mb-2 mt-2">
-          <AboutSection />
-          <TechnologySection />
-        </div>
+      <div className="relative z-10">
+        <Navbar />
 
-        <ExperienceSection />
-        <ProjectSection />
-        <HomeLabSection />
-        <CertificationSection />
-        <ContactSection />
+        <main className="flex-1 mb-20">
+          <BannerSection />
 
-      </main>
+          <div className="mb-2 mt-2">
+            <AboutSection />
+            <TechnologySection />
+          </div>
+
+          <ExperienceSection />
+          <ProjectSection />
+          <HomeLabSection />
+          <CertificationSection />
+          <ContactSection />
+
+        </main>
+      </div>
     </div>
   );
 }
